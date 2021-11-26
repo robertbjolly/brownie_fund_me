f47d3f427b9f021a6a4673089fe25ed8232dbb99
robertbjolly <robertbjolly@gmail.com>
2021-11-25 22:31:58 -0500

    first commit

---
+++ scripts/fund_and_withdraw.py
@@ -0,0 +1,25 @@
+from brownie import FundMe, MockV3Aggregator, network, config
+from scripts.helpful_scripts import (
+    get_account,
+    deploy_mocks,
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS
+)
+
+
+def fund():
+    fund_me = FundMe[-1]
+    account = get_account()
+    entrance_fee = fund_me.getEntranceFee()
+    print(f"The current entry fee is {entrance_fee}")
+    print("Funding")
+    fund_me.fund({"from": account, "value": entrance_fee})
+
+def withdraw():
+    fund_me = FundMe[-1]
+    account = get_account()
+    fund_me.withdraw({"from": account})
+
+def main():
+    fund()
+    withdraw()
+    
